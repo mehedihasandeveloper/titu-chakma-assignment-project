6682c7319f5d8d7bc6fb808acecffb747077c882
Mehedi Hasan <mh.ju1803@gmail.com>
2025-01-30 00:48:33 +0600

    issue solve

--- src/main/java/com/mehedihasandev/inventoryCRUD/WebConfig.java
+++ src/main/java/com/mehedihasandev/inventoryCRUD/WebConfig.java
@@ -2,6 +2,7 @@ package com.mehedihasandev.inventoryCRUD;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,4 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward all unmatched routes to index.html
+        registry.addViewController("/{path:[^\\.]*}").setViewName("forward:/index.html");
+    }
+
 }
